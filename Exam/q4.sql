aaa9bb269d927b0750c5157681bc40ebf1567a99
Yiheng (CSE) <y.quan@unsw.edu.au>
2019-05-06 22:00:55 +1000

    maybe the name oreder is wrong for q4

--- Exam/q4.sql
+++ Exam/q4.sql
@@ -1,5 +1,6 @@
 -- This one is interesting
 with Match as (select F.team as One, S.team as Two from Involves as F join Involves as S on
-F.match = S.match where F.team > S.team order by F.team, S.team)
-select One, Two, Count(*) as C from Match
-group by One, Two order by C desc;
+F.match = S.match where F.team > S.team),
+TopMatch as (select One, Two, Count(*) as C from Match group by One, Two)
+select F.country, S.country, T.C from Teams as F, Teams as S
+join TopMatch as T on T.One = F.id and T.Two = S.id and T.C = (select max(C) from TopMatch);
