f88dd3e7e8edc62b8738a838c11bd7da41d9bb41
Yiheng (CSE) <y.quan@unsw.edu.au>
2019-05-06 22:06:47 +1000

    lazy to change it

--- Exam/q4.sql
+++ Exam/q4.sql
@@ -1,6 +1,8 @@
 -- This one is interesting
-with Match as (select F.team as One, S.team as Two from Involves as F join Involves as S on
-F.match = S.match where F.team > S.team),
+drop view if exists q4;
+create view if not exists q4 as
+with Match as (select T.country as One, T.country as Two from Teams as T, Involves as F join Involves as S on
+F.match = S.match where T.country > T.country),
 TopMatch as (select One, Two, Count(*) as C from Match group by One, Two)
-select F.country, S.country, T.C from Teams as F, Teams as S
-join TopMatch as T on T.One = F.id and T.Two = S.id and T.C = (select max(C) from TopMatch);
+select T.One, T.Two, T.C from TopMatch as T
+join (select max(C) as most from TopMatch) as M on T.C = M.most;
